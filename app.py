d41d62c267478ce92c027818007cbf2c3b4db763
01fe22bcs148 <131373133+01fe22bcs148@users.noreply.github.com>
2025-04-06 13:40:09 +0530

    Added register.html and style.css with content

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, Render! Your Flask app is live 🚀"
+    return "Welcome to the Flask App on Render 🚀"
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        password = request.form['password']
+        return f"✅ Registered Successfully! <br>Name: {name}<br>Email: {email}"
+    return render_template('register.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=10000)
